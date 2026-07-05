18f943ad59ae21e1ec12c06c900b8e1b4755a36b
Alex Smirnov <alex-wd@yandex.ru>
2026-07-05 21:08:47 +0300

    Restaurant component refactoring

--- src/components/Restaurant/Restaurant.jsx
+++ src/components/Restaurant/Restaurant.jsx
@@ -5,8 +5,12 @@ export function Restaurant({ restaurant }) {
   return (
     <>
       <Title level={3}>{restaurant.name}</Title>
-      {restaurant.menu && <Title level={4}>Меню</Title>}
-      {restaurant.menu && <Menu menu={restaurant.menu} />}
+      {restaurant.menu && (
+        <>
+          <Title level={4}>Меню</Title>
+          <Menu menu={restaurant.menu} />
+        </>
+      )}
     </>
   );
 }
